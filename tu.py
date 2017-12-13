aa94bc6f01e987aa1d22e521f8ab9e8e9d73e153
hallbranden <30905002+hallbranden@users.noreply.github.com>
2017-12-13 08:01:07 -0800

    Add files via upload

---
+++ tu.py
@@ -0,0 +1,52 @@
+
+def greeting():
+    print("Good morning. You have been in suspension for -FIFTY- days. In compliance with state and federal regulations, all testing candidates in the Aperture Science Extended Relaxation Center must be revived periodically for a mandatory physical and mental wellness exercise.")
+    response=input('We will now take a simple wellnes exam. Are you ready (yes/no) ')
+    return response
+
+def second_choice():
+    print ("Great.....")
+    print ("First question:")
+    response=input('Does the Transitional Epithelium line the urinary bladder and is it capable of distention? (yes/no) ')
+    return response
+
+def haters():  #exits the game
+    print ("Be gone brainlet")
+    print ("*Fire burns you to death*")
+
+def opened():
+    print('Great! That was easy, wasnt it?')
+    print('Next question.....')
+    response=input('Swedish has three gramatical genders: Feminine, Masculine, and Neuter. (yes/no) ')
+    return response
+    print("you survived! congrats")
+
+def haters():  #exits the gamevndjvnuiaoshuidhauiwvyauidbwandnawnduwadhuwdiwajbuiabwvtyvbefgbswfevbesufnwdbsaiwodauwbdyawnduhawudy8gfueiyfbhsefbseifnbsdhvbwaljhdkbseifjeqfuqwhduaebwfjkrbgbdbgdrbgunsaefunsurgfuwanfkjdnjkwandjabwfbhugbaiuefhuiebgsguibsgeubghbsguiuiebuiadbuiwduabwduibawduibawdbwabduwabduwabduibsakdbksjdjgvdhtytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytytyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+    print ("Be gone brainlet")
+    print ("*Fire burns you to death*")
+    
+def second_choice():
+    print ("Great.....")
+    print ("First question:")
+    response=input('Does the Transitional Epithelium line the urinary bladder and is it capable of distention? (yes/no) ')
+    return response
+
+    #Enter your code here
+def not_opened():
+    print("Be gone thot")
+    print ("*Spikes inpale you causing to bleed out till death slowly*")
+    #Enter your code here
+
+    
+x = greeting()
+if x=="yes":
+    y = second_choice()
+    if y=="yes":
+        opened()
+    else:
+        not_opened()
+else:
+    haters()
+
+
+  
